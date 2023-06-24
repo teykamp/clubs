693d59e7f0408203d40960770fd52b908725d275
eykamt <embergameslp@gmail.com>
2023-06-24 12:36:25 -0400

    create classes for each pattern

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,19 +1,69 @@
-export type Pattern = {
+type BasePattern = {
     name: string,
     displayName: string,
     color: [number, number, number],
-    bodyColor: boolean,
-    ringColor: boolean,
+    bodyColor: {
+        on: boolean,
+        speed: number,
+        duration: number,
+        color: [number, number, number],
+    },
+    ringColor: {
+        on: boolean,
+        speed: number,
+        duration: number,
+        color: [number, number, number],
+    },
     sparkleColor: {
         on: boolean,
         speed: number,
         duration: number,
+        color: [number, number, number],
     },
     flashColor: {
         on: boolean,
         speed: number,
         duration: number,
+        color: [number, number, number],
     },
     patternSpeed: number,
     colorCycleSpeed: number,
 }
+
+type BaseSmallPattern = {
+    name: string,
+    displayName: string,
+    sparkleColor: {
+        on: boolean,
+        speed: number,
+        duration: number,
+        color: [number, number, number],
+    },
+    flashColor: {
+        on: boolean,
+        speed: number,
+        duration: number,
+        color: [number, number, number],
+    },
+    patternSpeed: number
+}
+
+export type SolidColor = BasePattern
+export type PulsingColor = BasePattern
+export type Confetti = BasePattern
+// 
+export type MultiPole = BasePattern & {
+    secondaryColor: {
+        on: boolean,
+        speed: number,
+        duration: number,
+        color: [number, number, number],
+    }
+}
+export type VerticalWave = MultiPole
+export type Torch = MultiPole
+// 
+export type LinearRainbow = BaseSmallPattern
+export type VerticalRainbow = BaseSmallPattern
+export type BPM = BaseSmallPattern
+export type Juggle = BaseSmallPattern
