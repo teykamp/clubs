1937b821f6befe2cb2c9f67cf9d28e9439402499
eykamt <embergameslp@gmail.com>
2023-07-02 23:08:31 -0400

    Fix bugs with colors not matching

--- src/components/ColorInterface.vue
+++ src/components/ColorInterface.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
     <v-container class="d-inline">
-
-
-      <!-- <div v-if="colors.color">
-        <v-col 
-          v-for="(_, index) in Object.values(colors).pop()" 
-          :key="index"
+        <v-dialog
+          v-model="colorDialog"
+          width="auto"
         >
           <v-color-picker 
             :modes="['hsl']"
@@ -14,28 +11,18 @@
             :hide-inputs="true"
             :hide-sliders="true"
             :hide-canvas="true"
-            v-model="Object.values(colors).pop()[index]"
+            v-model="colors.color[colorIndex]"
           ></v-color-picker>
-        </v-col>
-      </div>
-      <div v-else>
-        <v-col 
-          v-for="(_, index) in Object.values(colors).pop().color" 
-          :key="index"
-        >
-          <v-color-picker 
-            :modes="['hsl']"
-            :show-swatches="true"
-            :hide-inputs="true"
-            :hide-sliders="true"
-            :hide-canvas="true"
-            v-model="Object.values(colors).pop().color[index]"
-          ></v-color-picker>
-        </v-col>
-      </div>
-      {{ colors }} -->
-
-     
+          <v-card>
+            
+            <v-card-actions class="d-flex justify-center">
+              <v-btn 
+                color="primary" 
+                @click="colorDialog = false"
+              >OK</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
       <v-card elevation="0" class="mx-6">
         <v-card-title>{{ colors.name }}</v-card-title>
@@ -44,19 +31,37 @@
             :icon="linkColors ? 'mdi-link' : 'mdi-link-off'"
             variant="plain"
             density="compact"
-            @click="handleLinkColors()"
+            @click="linkColors = !linkColors"
           ></v-btn>
         </v-card-actions>
-        <div class="d-flex justify-space-around">
+        <div v-if="linkColors" class="d-flex justify-space-around">
           <div 
-            v-for="(_, index) in displayColors" 
+            v-for="(_, index) in colors.color.slice(1)" 
             :key="index"
           >
             <v-btn :style="{'background': `hsl(
-              ${displayColors[index].h} 
-              ${displayColors[index].s*100}% 
-              ${displayColors[index].l*100}%
-              )`}"></v-btn>
+              ${colors.color[index+1].h} 
+              ${colors.color[index+1].s*100}% 
+              ${colors.color[index+1].l*100}%
+              )`}"
+              @click="colorDialog = true;
+                      colorIndex = index + 1"
+            ></v-btn>
+          </div>
+        </div>
+        <div v-else class="d-flex justify-space-around">
+          <div 
+            v-for="(_, index) in colors.color.slice(2, -1)" 
+            :key="index"
+          >
+            <v-btn :style="{'background': `hsl(
+              ${colors.color[index].h} 
+              ${colors.color[index].s*100}% 
+              ${colors.color[index].l*100}%
+              )`}"
+              @click="colorDialog = true;
+                      colorIndex = 0"
+            ></v-btn>
           </div>
         </div>
       </v-card>
@@ -81,15 +86,8 @@ const colors = computed({
   set: (value) => emits("update:colors", value)
 })
 
-const linkColors = ref(true)
-const displayColors = ref(props.colors.color.slice(2, -1))
-function handleLinkColors() {
-  if (linkColors.value) {
-    displayColors.value = props.colors.color.slice(1)
-  } else {
-    displayColors.value = props.colors.color.slice(2, -1)
-  }
+const colorDialog = ref(false)
+const colorIndex = ref(0)
 
-  linkColors.value = !linkColors.value
-}
+const linkColors = ref(true)
 </script>
--- src/components/PatternInterface.vue
+++ src/components/PatternInterface.vue
@@ -11,35 +11,6 @@
     </div>
     <v-expand-transition>
       <v-card v-show="expand">
-        <!-- <v-dialog
-          v-model="dialog"
-          width="auto"
-          persistent
-        >
-          <v-card>
-            <v-card-text>
-              Linking colors removes any individually assigned colors. Continue?
-            </v-card-text>
-            <v-card-actions class="d-flex justify-center">
-              <v-btn 
-                color="primary" 
-                @click="dialog = false"
-              >OK</v-btn>
-              <v-btn 
-                color="red" 
-                @click="handleDialogCancel()"
-              >Cancel</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-switch
-          v-model="linkColors"
-          color="primary"
-          label="Link Colors"
-          @click="handleDialogOpen()"
-        >
-        </v-switch> -->
-
         <div 
           v-for="(_, key) in colorInterfaceColors"
           :key="key"
@@ -48,26 +19,8 @@
             v-model:colors="colorInterfaceColors[key]"
             :patternType="key"
           />
+
         </div>
-
-
-        <!-- <v-color-picker 
-          :modes="['hsl']" 
-          v-model="masterColor"
-        ></v-color-picker>
-        <div v-show="!linkColors">
-          <v-color-picker 
-            :modes="['hsl']" 
-            v-model="data.sparkleColor.color[1]"
-          ></v-color-picker>
-          <v-color-picker 
-            :modes="['hsl']" 
-            v-model="data.sparkleColor.color[2]"
-          ></v-color-picker>
-        </div>
-        {{ data.sparkleColor.color }} -->
-
-
       </v-card>
     </v-expand-transition>
   </div>
@@ -112,34 +65,6 @@ const data = computed({
   get: () => props.data,
   set: (value) => emits("update:data", value)
 })
-
-// const linkColors = ref(true)
-// const masterColor = ref({
-//   'h': 0,
-//   's': 0,
-//   'l': 0
-// })
-
-// watch(masterColor, (newColor) => {
-//   if (!linkColors.value) {
-//     props.data.sparkleColor.color[0] = newColor
-
-//   } else {
-//     props.data.sparkleColor.color = [newColor, newColor, newColor]
-//   }
-// })
-
-// const dialog = ref(false)
-// function handleDialogOpen() {
-//   if (!linkColors.value) {
-//     dialog.value = true
-//   }
-// }
-// function handleDialogCancel() {
-//   dialog.value = false
-//   linkColors.value = false
-// }
-
 const colorInterfaceColors = ref(getColorInterfaceColors())
 function getColorInterfaceColors() {
   let colors: object = new Object
