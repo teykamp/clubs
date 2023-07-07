c95101c7f748fd7e89b41e4a8474c5c879659285
eykamt <embergameslp@gmail.com>
2023-07-06 23:38:26 -0400

    add color chips to summary

--- src/components/PatternInterface.vue
+++ src/components/PatternInterface.vue
@@ -6,6 +6,30 @@
     >
       <v-chip color="primary">{{ chip }}</v-chip>
     </div>
+
+    <div v-if="'color' in data">
+      <div v-if="data.color.linkColors">
+        <v-chip :style="{'background': `hsl(
+                  ${data.color.color[0].h} 
+                  ${data.color.color[0].s*100}% 
+                  ${data.color.color[0].l*100}%
+                  )`,
+                  'width': '30px'}"
+        ></v-chip>
+      </div>
+      <div v-else>
+        <v-chip
+          v-for="color in data.color.color.slice(1)"
+          :key="color"
+          :style="{'background': `hsl(
+                  ${color.h} 
+                  ${color.s*100}% 
+                  ${color.l*100}%
+                  )`,
+                  'width': '30px'}"
+        ></v-chip>
+      </div>
+    </div>
     <div class="d-flex justify-end">
       <v-btn
         variant="plain"
