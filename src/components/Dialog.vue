9e422a0ba2ec1ef6aa756e76a667ab9ddf9c155a
eykamt <embergameslp@gmail.com>
2023-07-15 11:56:31 -0400

    add slot actions for dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -6,6 +6,9 @@
       >
       <v-card>
         <slot name="content"></slot>
+        <v-card-actions class="d-flex justify-center">
+         <slot name="actions"></slot>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
